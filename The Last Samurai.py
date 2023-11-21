fa4cae36c152d51d29f71d00d8bf0dd88d152b94
JamieC999 <jcousins286152@gmail.com>
2023-11-21 18:09:21 -0500

    I added the final touches to my python because it needed to be finished

--- The Last Samurai.py
+++ The Last Samurai.py
@@ -2,14 +2,21 @@ import random
 
 playAgain = True
 
+endingMessages = [
+    "Thank you for playing my game",
+    "Look out for more",
+    "Bye!!!"
+]
+
 # This function displays a start combat message
 def startCombat():
-    print( "Let's start with combat" )
+    print( "Let's fight, when i win comming for your army and for the country and taking it back" )
+
 
 
 startingStory = "\n6 enimies arrive to your country to take over, you arrived and the people need your help while you are the last samurai of your clan"
 
-civilianQuestion = "Who are you?...."
+civilianQuestion = "\nWho are you?...."
 Storyanswers = 'Im a samurai from the Hinokami clan, and im the last one.'
 
 
@@ -18,26 +25,46 @@ enemieName = 'Lord Muzan'
 print( startingStory )
 userName = input( f"{civilianQuestion}: " )
 
-print(f"Lord Muzan: Hahahahahah {userName}, you think you're going to win? No.")
+# While the user wants to play again, start again from here.
+while playAgain:
+    print(f"\nLord Muzan: Hahahahahah {userName}, you think you're going to win? No you will never win.")
 
-startCombat()
+    startCombat()
 
 
-userAttack = input( "Press enter to attack" )
-number1 = random.randint(0,1)
+    userAttack = input( "\nPress enter to attack [HIT ENTER]: " )
+    number1 = random.randint(0,1)
 
-userAttack = input( "Press enter to attack again " )
-number2 = random.randint(0,1)
+    print( "\nLord Muzan: Ouch!!!\n" )
 
-userAttack = input( "Press enter to attack one more time" )
-number3 = random.randint(0,1)
+    userAttack = input( "Press enter to attack again to get a potential crit: " )
+    number2 = random.randint(0,1)
 
-result = number1 + number2 + number3
+    print( "\nLord Muzan: So you've chosen death. Hit me again if you dare!!!!\n" )
 
-if result > 1:
-    print( f"{userName}: Lord Muzan, I beat you, hahahahahahaha" )
-else:
-    print( f"Lord Muzan, {userName} I beat you bad hahahahahaha" )
+    userAttack = input( "Press enter to attack one more time and hope Muzan will go down: " )
+    number3 = random.randint(0,1)
+
+    print( "\nLord Muzan: Oh, no you want trouble huh\n" )
+
+    result = number1 + number2 + number3
+
+    if result > 1:
+        print( f"{userName}: Lord Muzan, I beat you, and im taking my country back!" )
+    else:
+        print( f"Lord Muzan, {userName} I have defeted you and your country is mine hahah" )
+    
+    userResponse = input( "Do you want to fight again: y/n: " )
+
+    # if the response is not equal to a yes
+    if userResponse != "y":
+        playAgain = False
+
+# print out an empty line. Print out nothing and end with a new line
+print()
+
+for message in endingMessages:
+    print( message )
 
 
 
